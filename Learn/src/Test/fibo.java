e6c5790ee48401b6be1ebdf1393bb2b9c1d7a062
theyo <theyogeshkumar9116@gmail.com>
2021-06-20 12:28:38 +0530

    fibonnaci series without usisng any special command

---
+++ Learn/src/Test/fibo.java
@@ -0,0 +1,24 @@
+package Test;
+
+import java.util.Scanner;
+
+public class fibo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=0,b=1,c=0;
+		int n=sc.nextInt();
+		System.out.println(a);
+		System.out.println(b);
+		for(int i=0;i<n-2;i++) {
+			c=a+b;
+			System.out.println(c);
+			a=b;
+			b=c;
+			
+		}
+
+	}
+
+}
