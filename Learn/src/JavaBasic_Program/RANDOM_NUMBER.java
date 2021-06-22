1447cf54152d26ce409570452bb771ed40906ad4
theyo <theyogeshkumar9116@gmail.com>
2021-06-22 10:03:51 +0530

    printting random numbers using math random function

---
+++ Learn/src/JavaBasic_Program/RANDOM_NUMBER.java
@@ -0,0 +1,11 @@
+package JavaBasic_Program;
+
+public class RANDOM_NUMBER {
+	
+	public static void main(String[] args) {
+		System.out.println("the first random number:"+Math.random());
+		System.out.println("the second random number:"+Math.random());
+		System.out.println("the third random number:"+Math.random());
+	}
+
+}
