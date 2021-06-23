9b18dd1da80573b504a4ae2d5493d44b74ead0ac
theyo <theyogeshkumar9116@gmail.com>
2021-06-23 12:34:23 +0530

    reversing a string

--- Learn/src/JAVA_NUMBER_PROGRAMS/Reversing_String.java
+++ Learn/src/JAVA_NUMBER_PROGRAMS/Reversing_String.java
@@ -6,21 +6,19 @@ public class Reversing_String {
 	static String s;
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a string:");
-		s=sc.nextLine();
-		int a=s.length();
-		String b = null;
-		System.out.println("The length of the string:"+a);
-	     char A[]=new char[a];
-	     int i=0;
-	     a=a-1;
-		for( i=0;i<=a;i++)
-		{  char v =s.charAt(i);
-			A[a-i]=v;
-			
-//			System.out.println(v);
-		}  
+		s = sc.nextLine();
+		int a = s.length();
+		System.out.println("The length of the string is :" + a);
+		char A[] = new char[a];
+		int i;
+		a = a - 1;
+		for (i = 0; i <= a; i++) {
+			char v = s.charAt(i);
+			A[a - i] = v;
+		}
+
 		System.out.println(A);
 	}
-	}
+}
