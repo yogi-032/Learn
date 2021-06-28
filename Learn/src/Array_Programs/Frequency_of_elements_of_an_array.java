e61f82b3ee3f3fd2667bf6bac231d51ebc67fcdc
theyo <theyogeshkumar9116@gmail.com>
2021-06-28 11:44:21 +0530

    frequency of elements in a array

--- Learn/src/Array_Programs/Frequency_of_elements_of_an_array.java
+++ Learn/src/Array_Programs/Frequency_of_elements_of_an_array.java
@@ -3,26 +3,37 @@ package Array_Programs;
 import java.util.Scanner;
 
 public class Frequency_of_elements_of_an_array {
-
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a string");
-		String s=sc.nextLine();
-		System.out.println("Enter a charcter you want to know the occurence: ");
-		char c=sc.next().charAt(0);
-		int l=s.length();
-		int count=0;
-		for(int i=0;i<l;i++) {
-			char d=s.charAt(i);
-			if(d==c) {
-				count++;
-			}
-			else {
-				continue;
-			}
-		}
-		System.out.println("The character you have inserted is occuring  " + count+" times.");
-
-	}
-
+           public static void main(String[] args) {
+        	   Scanner sc=new Scanner(System.in);
+        	   System.out.println("Enter the size of the array:");
+        	   int a=sc.nextInt();
+        	   int[] A=new int[a];
+        	   int[] B=new int[a];
+        	   System.out.println("Enter the elements of the array");
+        	  for(int i=0;i<a;i++) {
+        		  A[i]=sc.nextInt();
+        	  }
+        	  int visited=-1;
+        	
+        	  for(int i=0;i<a;i++) {
+        		  int count=1;
+        		  boolean flag=true;
+        		  for(int j=i+1;j<a;j++) {
+        			  if(A[i]==A[j]) {
+        				  count++;
+        				  B[j]=visited;
+        			  }
+        			  
+        		  }
+        		  if(B[i]!=visited) {
+        			  B[i]=count;
+        		  }
+        	  }
+        	  for(int i=0;i<a;i++) {
+        		  if(B[i]!=visited) {
+        			  System.out.println(A[i]+"|"+B[i]);
+        		  }
+        	  }
+        	   
+           }
 }
