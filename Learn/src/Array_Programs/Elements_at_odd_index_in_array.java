033c34db415c69e7a80ec80c33b1296312a73331
theyo <theyogeshkumar9116@gmail.com>
2021-06-28 21:47:21 +0530

    Printing the elements of the array at odd positions.

---
+++ Learn/src/Array_Programs/Elements_at_odd_index_in_array.java
@@ -0,0 +1,24 @@
+package Array_Programs;
+
+import java.util.Scanner;
+
+public class Elements_at_odd_index_in_array {
+
+	public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of an array");
+        int a=sc.nextInt();
+        System.out.println("Enter the elements of the array");
+       int A[]=new int[a];
+       for(int i=0;i<a;i++) {
+    	   A[i]=sc.nextInt();
+       }
+       for(int i=0;i<a;i++) {
+    	   if(i%2==0) {
+    		    System.out.println(A[i]);
+    	   }
+       }
+
+	}
+
+}
