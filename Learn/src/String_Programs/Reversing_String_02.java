a8087be5fe49fcf505f7b6decfbc97f70864fa38
theyo <theyogeshkumar9116@gmail.com>
2021-06-29 21:17:46 +0530

    Reversing the string

---
+++ Learn/src/String_Programs/Reversing_String_02.java
@@ -0,0 +1,22 @@
+package String_Programs;
+
+import java.util.Scanner;
+
+public class Reversing_String_02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string:");
+		String s = sc.nextLine();
+		int a = s.length();
+		System.out.println("The length of the string is :" + a);
+		int i;
+		a = a - 1;
+		for (i = a; i >= 0; i--) {
+			char v = s.charAt(i);
+			System.out.print(v);
+		}
+			
+		}
+	}
+
