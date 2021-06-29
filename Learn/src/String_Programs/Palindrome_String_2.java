df7863bdd354d9eeb7ff158bb1463526d3e8f84b
theyo <theyogeshkumar9116@gmail.com>
2021-06-29 21:14:11 +0530

    To check whether a string is palindrome or not:

---
+++ Learn/src/String_Programs/Palindrome_String_2.java
@@ -0,0 +1,28 @@
+package String_Programs;
+
+import java.util.Scanner;
+
+public class Palindrome_String_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          Scanner sc=new Scanner(System.in);
+          System.out.println("Enter a string.");
+          String s=sc.nextLine();
+          boolean flag=true;
+          s=s.toLowerCase();
+          for(int i=0;i<s.length()/2;i++) {
+        	 if(s.charAt(i)!=s.charAt(s.length()-i-1)) {
+        		 flag=false;
+        		 break;
+        	 }
+        	 
+          }if(flag) {
+         	 System.out.println("The string is  a palindrome");
+        	 
+           }else {
+         	  System.out.println("The string is not a palindrome.");
+           }
+	}
+
+}
