2b0835f66e9f44014cf4c894d3e40aa4ea3317d1
theyo <theyogeshkumar9116@gmail.com>
2021-06-29 07:48:08 +0530

    Sorting the elements in ascending order

---
+++ Learn/src/Array_Programs/sorting_elements_in_ascending_order.java
@@ -0,0 +1,30 @@
+package Array_Programs;
+
+import java.util.Scanner;
+
+public class sorting_elements_in_ascending_order {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of the array");
+		int a =sc.nextInt();
+		System.out.println("Enter the elements of the array");
+		int[] A=new int[a];
+		for(int i=0;i<a;i++) {
+			A[i]=sc.nextInt();
+		}
+     for(int i=0;i<a;i++) {
+    	 for(int j=i+1;j<a;j++) {
+    		 if(A[i]>A[j]) {
+    			 int temp=A[i];
+    			 A[i]=A[j];
+    			 A[j]=temp;
+    		 }	 
+    	 }
+       }
+     for(int x:A) {
+    	 System.out.println(x);
+     }
+	}
+
+}
