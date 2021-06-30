f77624c29df29cefe8a5d6d9ebb7f4bf0b54a191
theyo <theyogeshkumar9116@gmail.com>
2021-06-30 08:16:28 +0530

    Removing all the white spaces.

---
+++ Learn/src/String_Programs/replacing_space_with_.java
@@ -0,0 +1,22 @@
+package String_Programs;
+
+import java.util.Scanner;
+
+public class replacing_space_with_ {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter any string.");
+		String s=sc.nextLine();
+		for(int i=0;i<s.length();i++) {
+			if(s.charAt(i)==' ') {
+				s=s.replace(' ', '_');
+			}
+		}
+    System.out.println(s);
+    // or we can use another in built method as s=s.replaceAll(' ','-');
+    //        str1 = str1.replaceAll("\\s+", "");-This will remove all the white spaces    
+
+	}
+
+}
